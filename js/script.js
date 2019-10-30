a0dfa762f1e8b59a134409e68eb0b870de14b4b0
YonglinMai <yonglinmai@gmail.com>
2019-10-30 22:28:11 +0000

    does not word

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("#button").click(function(){
+    let word = $(".input").val();
+    let vowel = "aeiou"
+    if (vowel.includes(word[0]))
+        return word = word + "ay";
+    $(".output").append(word)
 
-
-
-  
-  
-  
-  
+});
 });
